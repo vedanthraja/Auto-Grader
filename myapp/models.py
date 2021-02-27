5cc8adf6bb31290c7971007bd2fe156e464e5a17
shubhamthakar <62199528+shubhamthakar@users.noreply.github.com>
2021-02-27 20:09:05 +0530

    Added- Login, Register, Logout

--- myapp/models.py
+++ myapp/models.py
@@ -5,13 +5,35 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-
 class CustomUser(AbstractUser):
     is_teacher = models.BooleanField(default = 1)
 
+class Teacher(models.Model):
+    username = models.CharField(unique=True, max_length = 30)
+    password = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 30)
+    subject = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    username = models.CharField(unique=True, max_length = 30)
+    password = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 30, null = True)
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
 class Quiz(models.Model):
     quiz_id = models.AutoField(primary_key = True)
-    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    student = models.ForeignKey(Student, on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.quiz_id
 
 class Question(models.Model):
     ques_text = models.TextField()
@@ -19,7 +41,9 @@ class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete = models.CASCADE)
 
 class Answer(models.Model):
-    # student = models.ForeigKey(student)
+    student = models.ForeignKey(Student, on_delete = models.CASCADE, null = True)
     image = models.FileField(upload_to='uploads/')
     ans_text = models.TextField()
+    grade = models.DecimalField(max_digits=3, decimal_places=0, null=True)
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
+
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('registerStudent/',views.RegisterStudent, name="RegisterStudent"),
+    path('registerTeacher/',views.RegisterTeacher, name="RegisterTeacher"),
+    path('index/',views.index, name="index"),
+    path('login/',views.signin, name="login")
 ]
